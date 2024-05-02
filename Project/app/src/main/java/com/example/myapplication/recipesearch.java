06ac363a08247fdcca7f6335631f07d03af14f2c
Yue Yu <u7709381@anu.edu.au>
2024-05-02 12:28:25 +1000

    fuzzy search

--- Project/app/src/main/java/com/example/myapplication/recipesearch.java
+++ Project/app/src/main/java/com/example/myapplication/recipesearch.java
@@ -24,7 +24,7 @@ import java.util.ArrayList;
 import java.util.List;
 import org.json.JSONArray;
 import org.json.JSONObject;
-import org.apache.commons.text.similarity.LevenshteinDistance;
+
 
 public class recipesearch extends AppCompatActivity {
     private ArrayList<String> recipes = new ArrayList<>();
@@ -104,22 +104,47 @@ public class recipesearch extends AppCompatActivity {
     }
 
 
-
     private List<String> getRecipesWithIngredient(String ingredient) {
         List<String> filteredRecipes = new ArrayList<>();
         String ingredientLowerCase = ingredient.toLowerCase();
-        LevenshteinDistance distance = LevenshteinDistance.getDefaultInstance();
-
         for (String recipe : recipes) {
-            String recipeLowerCase = recipe.toLowerCase();
-            int levenshteinDistance = distance.apply(ingredientLowerCase, recipeLowerCase);
-            if (levenshteinDistance <= 2) {
+            // Calculate the similarity between the recipe name and the target ingredient
+            double similarity = calculateSimilarity(recipe.toLowerCase(), ingredientLowerCase);
+            // If similarity reaches threshold, add recipe to results
+            if (similarity >= 0.6) { // May change
                 filteredRecipes.add(recipe);
             }
         }
         return filteredRecipes;
     }
-}
+
+    private double calculateSimilarity(String s1, String s2) {
+        int editDistance = editDistance(s1, s2);
+        int maxLength = Math.max(s1.length(), s2.length());
+        return 1.0 - ((double) editDistance / maxLength);
+    }
+
+    private int editDistance(String s1, String s2) {
+        int m = s1.length();
+        int n = s2.length();
+        int[][] dp = new int[m + 1][n + 1];
+
+        for (int i = 0; i <= m; i++) {
+            for (int j = 0; j <= n; j++) {
+                if (i == 0) {
+                    dp[i][j] = j;
+                } else if (j == 0) {
+                    dp[i][j] = i;
+                } else if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = 1 + Math.min(dp[i - 1][j - 1], Math.min(dp[i - 1][j], dp[i][j - 1]));
+                }
+            }
+        }
+
+        return dp[m][n];
+    }
 
 
 
