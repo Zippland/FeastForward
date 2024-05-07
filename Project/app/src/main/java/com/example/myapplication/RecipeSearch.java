a7af91ed7ae787d86ffef7ed125e811a4c0c4ed4
Zhongyi <u7619253@anu.edu.au>
2024-05-08 02:14:04 +1000

    rename recipesearch

--- Project/app/src/main/java/com/example/myapplication/RecipeSearch.java
+++ Project/app/src/main/java/com/example/myapplication/RecipeSearch.java
@@ -2,14 +2,8 @@ package com.example.myapplication;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
-import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -27,7 +21,7 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 
 
-public class recipesearch extends AppCompatActivity {
+public class RecipeSearch extends AppCompatActivity {
     private ArrayList<String> recipes = new ArrayList<>();
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
