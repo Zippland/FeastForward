8daa3f67e212e56092cecf3b9d216289aafcec81
Zihan <u7174903@anu.edu.au>
2024-05-12 18:24:29 +1000

    calendar view

--- Project/app/src/main/java/com/example/myapplication/DataActivity.java
+++ Project/app/src/main/java/com/example/myapplication/DataActivity.java
@@ -8,8 +8,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.app.DatePickerDialog;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
 
 import com.example.myapplication.Utils.DataUtil;
+
+import java.util.Calendar;
 
 public class DataActivity extends AppCompatActivity {
 
@@ -24,19 +30,21 @@ public class DataActivity extends AppCompatActivity {
 
         DataUtil.init(this);
         recipeInput = findViewById(R.id.recipe_input);
-        expiredInput = findViewById(R.id.expired_input);
+        TextView expiredDisplay = findViewById(R.id.expired_display);
+
+        Button expiredButton = findViewById(R.id.expired_button);
+        expiredButton.setOnClickListener(v -> showDatePickerDialog(expiredDisplay));
 
         //add data
         Button addButton = findViewById(R.id.add_button);
         addButton.setOnClickListener(v -> {
-            View rootView = getWindow().getDecorView().getRootView();
             String recipeName = recipeInput.getText().toString();
-            String expiredTime = expiredInput.getText().toString();
-            if (!recipeName.isEmpty() && !expiredTime.isEmpty()) {
+            String expiredTime = expiredDisplay.getText().toString();
+            if (!recipeName.isEmpty() && !expiredTime.equals("Select Date")) {
                 addRecipe(recipeName, expiredTime, 1);
                 //clear input
                 recipeInput.setText("");
-                expiredInput.setText("");
+                expiredDisplay.setText("Select Date");
                 showContextMenu("Data added successfully！");
             } else {
                 showContextMenu("Mandatory fields cannot be blank");
@@ -74,4 +82,18 @@ public class DataActivity extends AppCompatActivity {
         dialog.show();
     }
 
+    private void showDatePickerDialog(final TextView display) {
+        final Calendar c = Calendar.getInstance();
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH);
+        int day = c.get(Calendar.DAY_OF_MONTH);
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(this,
+                (view, year1, monthOfYear, dayOfMonth) -> {
+                    // format the display of date with leading zeros for month and day
+                    String formattedDate = String.format("%d-%02d-%02d", year1, monthOfYear + 1, dayOfMonth);
+                    display.setText(formattedDate);
+                }, year, month, day);
+        datePickerDialog.show();
+    }
 }
