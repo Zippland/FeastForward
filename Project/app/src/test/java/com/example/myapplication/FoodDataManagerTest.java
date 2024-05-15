32f6c9e703e468e22fb4e87b12b14ce41185b63e
Baizhen Lin <scybl2@nottingham.edu.cn>
2024-05-15 22:04:32 +1000

    modify the test code

--- Project/app/src/test/java/com/example/myapplication/FoodDataManagerTest.java
+++ Project/app/src/test/java/com/example/myapplication/FoodDataManagerTest.java
@@ -19,7 +19,7 @@ import java.util.Locale;
 /**
  * Unit tests for FoodDataManager class.
  *
- * @author Baizhen Lin
+ * @autor Baizhen Lin
  */
 public class FoodDataManagerTest {
 
@@ -129,5 +129,122 @@ public class FoodDataManagerTest {
         List<String[]> userData = foodDataManager.getUserData(new File("dummy"));
         assertEquals(1, userData.size());
         assertEquals("Banana", userData.get(0)[0]);
+    }
+
+    /**
+     * Tests retrieval of food data for an invalid user.
+     *
+     * @throws IOException if an I/O error occurs
+     */
+    @Test
+    public void testGetUserDataInvalidUser() throws IOException {
+        foodDataManager = new FoodDataManager(
+                999, // Invalid userId
+                file -> new BufferedReader(new StringReader(sampleCsvData)),
+                file -> new BufferedWriter(stringWriter)
+        );
+        List<String[]> userData = foodDataManager.getUserData(new File("dummy"));
+        assertEquals(0, userData.size());
+    }
+
+    /**
+     * Tests retrieval of food items not near expiry.
+     *
+     * @throws IOException if an I/O error occurs
+     * @throws ParseException if the date format is invalid
+     */
+    @Test
+    public void testGetNoNearExpiryItems() throws IOException, ParseException {
+        String notNearExpiryCsvData = "Food Name,Expiry Date,UserId,IsShared,UserName\n" +
+                "Apple,2024-06-10,1,no,User1\n" +
+                "Banana,2024-06-12,1,yes,User1\n" +
+                "Carrot,2024-06-11,2,no,User2";
+
+        foodDataManager.readerProvider = file -> new BufferedReader(new StringReader(notNearExpiryCsvData));
+
+        List<String[]> nearExpiryItems = foodDataManager.getNearExpiryItems(new File("dummy"));
+        assertEquals(0, nearExpiryItems.size());
+    }
+
+    /**
+     * Tests retrieval of shared data including items shared by others.
+     *
+     * @throws IOException if an I/O error occurs
+     */
+    @Test
+    public void testGetSharedDataIncludesOthers() throws IOException {
+        String sharedDataCsv = "Food Name,Expiry Date,UserId,IsShared,UserName\n" +
+                "Apple,2024-05-10,1,no,User1\n" +
+                "Banana,2024-05-12,1,yes,User1\n" +
+                "Carrot,2024-05-11,2,yes,User2\n" +
+                "Date,2024-05-12,3,yes,User3";
+
+        foodDataManager.readerProvider = file -> new BufferedReader(new StringReader(sharedDataCsv));
+
+        List<String[]> sharedData = foodDataManager.getSharedData(new File("dummy"));
+        assertEquals(2, sharedData.size());
+        assertEquals("Carrot", sharedData.get(0)[0]);
+        assertEquals("Date", sharedData.get(1)[0]);
+    }
+
+    /**
+     * Tests retrieval of empty user data.
+     *
+     * @throws IOException if an I/O error occurs
+     */
+    @Test
+    public void testGetUserDataEmpty() throws IOException {
+        String emptyCsvData = "Food Name,Expiry Date,UserId,IsShared,UserName\n";
+
+        foodDataManager.readerProvider = file -> new BufferedReader(new StringReader(emptyCsvData));
+
+        List<String[]> userData = foodDataManager.getUserData(new File("dummy"));
+        assertEquals(0, userData.size());
+    }
+
+    /**
+     * Tests retrieval of user data with missing fields.
+     *
+     * @throws IOException if an I/O error occurs
+     */
+    @Test
+    public void testGetUserDataMissingFields() throws IOException {
+        String missingFieldsCsvData = "Food Name,Expiry Date,UserId,IsShared,UserName\n" +
+                "Apple,2024-05-10,1,no\n" + // Missing UserName
+                "Banana,2024-05-12,1,yes,User1\n";
+
+        foodDataManager.readerProvider = file -> new BufferedReader(new StringReader(missingFieldsCsvData));
+
+        List<String[]> userData = foodDataManager.getUserData(new File("dummy"));
+        assertEquals(1, userData.size());
+        assertEquals("Banana", userData.get(0)[0]);
+    }
+
+    /**
+     * Tests updating the shared status of a non-existing food item.
+     *
+     * @throws IOException if an I/O error occurs
+     */
+    @Test
+    public void testUpdateFoodSharedStatusNonExisting() throws IOException {
+        foodDataManager.updateFoodSharedStatus(new File("dummy"), "NonExistingFood");
+        List<String[]> userData = foodDataManager.getUserData(new File("dummy"));
+        assertEquals(2, userData.size());
+        assertEquals("no", userData.get(0)[3]);
+        assertEquals("yes", userData.get(1)[3]);
+    }
+
+    /**
+     * Tests deleting a non-existing food item.
+     *
+     * @throws IOException if an I/O error occurs
+     */
+    @Test
+    public void testDeleteFoodItemNonExisting() throws IOException {
+        foodDataManager.deleteFoodItem(new File("dummy"), "NonExistingFood");
+        List<String[]> userData = foodDataManager.getUserData(new File("dummy"));
+        assertEquals(2, userData.size());
+        assertEquals("Apple", userData.get(0)[0]);
+        assertEquals("Banana", userData.get(1)[0]);
     }
 }
